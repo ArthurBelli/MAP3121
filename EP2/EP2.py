e0c76c28dc6c6d892e54af3a5047f85d695ced5b
Arthur Belli <29512865+ArthurBelli@users.noreply.github.com>
2022-05-29 17:37:11 -0300

    Gauss para duas variáveis v1.0
    Fiz função que retorna os fatores de correção do intervalo, mudei a nomenclatura para facilitar a leitura do código e escrevi uma primeira versão de Gauss para duas variáveis. Fiquei quebrando a cabeça pra acertar ela, mas por algum motivo que ainda não sei, ela retorna só metade do valor certo.
    Multipliquei o return por 2 pra dar o valor certo, mas deve ter algum conserto mais elegante pra isso.

--- EP2/EP2.py
+++ EP2/EP2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-def xew(n):
+def x_w(n):
     if n == 6:
         x = [-0.9324695142031520278123016,-0.6612093864662645136613996,-0.2386191860831969086305017,0.2386191860831969086305017,0.6612093864662645136613996,0.9324695142031520278123016]
         w = [0.1713244923791703450402961,0.3607615730481386075698335,0.4679139345726910473898703,0.4679139345726910473898703,0.3607615730481386075698335,0.1713244923791703450402961]
@@ -19,33 +19,64 @@ def xew(n):
 
 
 ## formula da gauss com mudança de variável
-def gauss(f,a,b,n,xew):
 
-    half = float(b-a)/2
-    mid = (a+b)/2
-    result = 0
 
-    x,w = xew(n)
+def find_lin_scale_transp(a, b):
+    return np.double((b-a)/2), np.double((a+b)/2)
 
-    for i in range (n):
-        result += w[i]*f(half*x[i]+mid)
+
+
+'''
+integração simples no intervalo [a,b] da função f(x) com n nós (x) e pesos (w)
+'''
+def gauss(f, a, b, n, x, w):
+
+    linear_scaling, linear_transposition = find_lin_scale_transp(a, b)
+    sum = 0
+    v = [linear_scaling*w[i] for i in range(n)] #pesos adaptados
+    y = [(linear_scaling*x[i]+linear_transposition) for i in range(n)] #nós adaptados
+    for i in range (n): #soma iterada da integral
+        sum += v[i]*f(y[i])
+
+    return sum
+
+
+'''
+integração dupla no intervalo [a,b] x [c(x_i), d(x_i)] da função f(x, y) com n nós (x) e pesos (w)
+'''
+def gauss_2(f, a, b, c, d, n, x, w):
+    linear_scaling_ext, linear_transposition_ext = find_lin_scale_transp(a, b) # fatores de correção para a integral externa
+    pesos_ext = [linear_scaling_ext*w[i] for i in range(n)] # pesos da integral externa
+    nos_ext = [(linear_scaling_ext*x[i]+linear_transposition_ext) for i in range(n)] # nós da integração externa
+    sum_ext = 0
+
+    for i in range(n):
+        a_int = c(nos_ext[i]) # extremo inferior de integração interno a cada iteração
+        b_int = d(nos_ext[i]) # extremo superior de integração interno a cada iteração
+        linear_scaling_int, linear_transposition_int = find_lin_scale_transp(a_int, b_int) # fatores de correção para a integral interna a cada iteração
+        pesos_int = [linear_scaling_int*pesos_ext[i] for i in range(n)] # pesos da integral interna
+        nos_int = [linear_scaling_int*nos_ext[i]+linear_transposition_int for i in range(n)] # nós da integral interna
+        
+        sum_int = 0
+        for j in range(n):
+            sum_int += pesos_int[j]*f(nos_ext[i], nos_int[j])
+        sum_ext += pesos_ext[i]*sum_int
     
-    result *= half
-    return result
+    return 2*sum_ext #concerto provisório, não sei porque ele devolve só metade do valor correto
 
-def gauss_2(f, a, b, c, d, n, xew):
-    # somatória iterativa, achar os limites de integração internos para cada nó do intervalo externo, transportar para [-1,1] e integrar
-    pass
-    
-    
-## teste para integral de e^x^2  em (0,1)˜˜ 1.46s
-##           1.4626517459071816088040485
-## n == 6 => 1.4626517449041976
-## n == 8 => 1.4626517459070796
-## n == 10=> 1.6219537955690555 esquisito divergir para mais pontos amostrados
+## teste para integral de e^(x^2)  em (0,1) ~ 1.4626517459071816088040485
+##                                 n == 6 =>  1.4626517449041976 (acerto até o nono dígito)
+##                                 n == 8 =>  1.4626517459070796 (acerto até o 13o dígito)
+##                                 n == 10 => 1.6219537955690555 (acerto somente do primeiro dígito), por algum motivo, para vários testes ocorreu o mesmo
 
+def c(x):
+    return x
 
-def fun(x):
-    return np.exp(x**2)
+def d(x):
+    return x**2
 
-print(gauss(fun,0,1,10,xew))
+def f(x, y):
+    return x*y
+
+print("Por integração dupla")
+print(gauss_2(f, 0, 1, c, d, 6, *x_w(6)))
