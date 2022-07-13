f0f6da78ca3f92487d08e7e6d240b39aa3afba41
tichafurusawa07 <83107056+tichafurusawa07@users.noreply.github.com>
2022-07-13 15:55:21 -0300

    Update ep3Let.py

--- EP3/ep3Let.py
+++ EP3/ep3Let.py
@@ -4,87 +4,154 @@ EP3 - Modelagem de um Sistema de Resfriamento de Chips
 11804608 - Arthur Pedroso Porto Belli
 11965585 - Letícia Harumi Furusawa
 '''
+
 import numpy as np
-from MAP3121.EP3.utils import *#contém as funções inalteradas de EPs passados
+import matplotlib.pyplot as plt
+from utils import * #contém as funções inalteradas de EPs passados
 
-# função de excitação do sistema
-def f(x, y=0):
-    return 12*x*(1-x)-2
 
-def phi_i1(y, x): #y é a variável independente, x é o array de pontos e i é o índice desejado
-    return (y-x) #expressão para o trecho [x_{i-1}, x_i]
-
-def phi_i2(y, x):
-    return (x-y) # expressão de \phi para o trecho [x_i, x_{i+1}]
-
-def funcao1(f, y, z, x): #funcao a ser integrada deduzidda na secção de exercícios do relatório
-    return f(y)*phi_i1(y, x)
-
-def funcao2(f, y, z, x): #para serem compatíveis com o código desenvolvido para o EP2, precisamos de duas vars. independentes (y, z)
-    return f(y)*phi_i2(y, x)
+def zero(x):
+    return 0
 
 # condutividade térmica
+
 def k(x):
     return 1
 
-# Soma do calor gerado pelo chip com o calor retirado pelo resfriamento, no equilibrio eles são iguais. Assim, o calor interno não se altera
-def q(x):
-    return 0
+def q(Qp0, Qm0, sigma, theta, L, x):
+    q = Qp0*np.exp(-((x-L)/sigma)**2) - Qm0*(np.exp(-(x/theta)**2) + np.exp(-((x-L)/theta)**2))
+    return q
+
+# função de excitação do sistema
+def excitacao(x):
+    return 12*x*(1-x)-2
 
 
+def phi(x, apoios, h, i): #funções da base do espaço gerado para o método dos elementos finitos
+    if apoios[i-1] <= x <= apoios[i]: #primeira metade do spline é crescente
+        return (x-apoios[i-1])/h
+    elif apoios[i] <= x <= apoios[i+1]: #segunda metade do spline é decrescente
+        return (apoios[i+1]-x)/h
+    return 0 # fora do intervalo
 
-'''
-primeira implementação, estou tentando resolver o caso mais simples
-L = 1 k(x)=1 q(x)=0
-'''
 
-def fem(n):
-    h = 1/(n+1) #passo dos apoios
-    x = np.array([i*h for i in range(n+1)], dtype=np.double)
+def debug_phi(apoios, h, n):
+    for k in range(1, n+1):
+        print(f"phi_{k}_subida = (x-{apoios[k-1]}/{h}), para x in [{apoios[k-1]}, {apoios[k]}]")
+        print(f"phi_{k}_descida = ({apoios[k+1]} - x/{h}), para x in [{apoios[k]}, {apoios[k+1]}]")
 
+def calc_v(alphas, x, apoios, h, n):
+    temp = float(sum([alphas[i-1]*phi(x, apoios, h, i) for i in range(1, n+1)]))
+    return temp
+
+
+def fem(n, L, k, f):
+    '''
+    retorna a solução do problema normalizado em x in [0,1]
+
+    n -> número de apoios
+    L -> comprimento do chip
+    k -> condutividade térmica do material
+    f -> função de excitação do sistema
+    '''
+    h = L/(n+1) #passo dos apoios
+
+    print(f"h = {h}")
+
+    apoios = [i*h for i in range(n+2)]
+    print("apoios não normalizados: ")
+    print(apoios)
+
+    apoios_barr = [apoios[i]/L for i in range(n+2)]
+    print("apoios normalizados: ")
+    print(apoios_barr)
+    print("\n")
+
+    #debug_phi(apoios_barr, h, n)
     #definição da matriz normal do MMQ 
-    a = np.array([0 if i == 0 else (-1)*(1/h) for i in range(n)], dtype=np.double) #diagonal inferior
-    b = np.array([2/h for i in range(n)], dtype=np.double) #diagonal principal
-    c = np.array([0 if i == n-1 else (-1)*(1/h) for i in range(n)], dtype=np.double) #diagonal superior
+
+    #diagonal inferior
+    #print("Diagonal inferior")
+    a = [0 if i == 0 else (-1)*(1/(L*(h**2)))*gauss(k, apoios_barr[i], apoios_barr[i+1], 2, *x_w(2)) for i in range(n)] 
+    #print(a)
+    a = np.array(a, dtype=np.double)
+
+    #diagonal principal
+    #print("Diagonal principal")
+    b = [(1/(L*(h**2)))*(gauss(k, apoios_barr[i-1], apoios_barr[i], 2, *x_w(2)) + gauss(k, apoios_barr[i], apoios_barr[i+1], 2, *x_w(2))) for i in range(1, n+1)] 
+    #print(b)
+    b = np.array(b, dtype=np.double)
+
+    #diagonal superior 
+    #print("Diagonal superior")
+    c = [0 if i == n-1 else (-1)*(1/(L*(h**2)))*gauss(k, apoios_barr[i], apoios_barr[i+1], 2, *x_w(2)) for i in range(n)]
+    #print(c)
+    c = np.array(c, dtype=np.double)
 
     #array de termos independentes
-    #putaria do krl q eu tive q fazer aqui pra funcionar, mas acho q funciona.....
-    d = [(1/h)*(gauss_2((funcao1, f, 0, 1, x[i-1], x[i], 10, *x_w(10), "inf") + gauss_2(funcao2, f, 0, 1, x[i], x[i+1], 10, *x_w(10), "sup"))) for i in range(1, n)]
-    d = np.array(d, dtype=np.double)
-
-    #resolução do sistema linear
-    alphas = np.array(solve_lin_sys_tridiag(*A_to_LU_tridig(a, b, c), d), dtype=np.double)
-
-
-def temperatura(n): # calcula a temperatura pelo fem
-    return fem(n)
-
-def temperatura2(f,n,L): # calcula a temperatura pela equação f dada
+    #print("Termos independentes")
+    d = [(gauss((lambda nos: f(nos)*phi(nos, apoios, h, i)), apoios[i-1], apoios[i], 2, *x_w(2)) + gauss((lambda nos: f(nos)*phi(nos, apoios, h, i)), apoios[i], apoios[i+1], 2, *x_w(2))) for i in range(1, n+1)]
+    #print(d)
     
-    h= L/(n+1)
-    x = np.array([i*h for i in range(n+1)], dtype=np.double)
-    u = np.array([f(x[i]) for i in range (1,n+1)],dtype=np.double)
+    #resolução do sistema linear
+    #print("alphas")
+    alphas = solve_lin_sys_tridiag(*A_to_LU_tridig(a, b, c), d)
+    #print(alphas)
 
-    return u
+    eixox = np.linspace(0, L, 100)
+    v = [0]*len(eixox)
+    for k in range(len(eixox)):
+        v[k] = calc_v(alphas, eixox[k], apoios, h, n)
+    return v #vetor solução normalizado
 
-def erro_max(u_fem, L, n, u_teor): # devolve o erro maximo entre o valor encontrado pelo fem e o teórico
 
-    h=L/(n+1)
-    x = np.array([i*h for i in range(n+1)], dtype=np.double)
+def solve_generic(n, L, k, f, a, b): # calcula a temperatura pela equação f dada
+    '''
+    n -> número de apoios a serem utilizados
+    L -> comprimento do chip
+    k -> condutividade térmica do materia
+    f -> função de excitação térmica do chip
+    a -> condição de contorno no extremo 0
+    b -> condição de contorno no extremo L
+    '''
+
+    h = L/(n+1)
+    eixox = np.linspace(0, L, 100)
+    v = fem(n, L, k, f)
+    u = [0]*len(eixox)
+    for i in range(len(eixox)):
+        u[i] = (v[i] + a + (b-a)*eixox[i]/L)
+    return u #vetor solução individualizado
+
+
+def erro_max(u_teor, u_fem): # devolve o erro maximo entre o valor encontrado pelo fem e o teórico
     erro = u_teor - u_fem
     erro_m = np.max(erro)
 
     return erro_m
 
+def forcantes(q,L,n):
+  k=3.6
+  return fem(n,L,k,q)
 
+def mat_varia(q,L,n,d):
+  
+  k1=3.6
+  k2=60
 
-def solve():
-    pass
+  material1= solve_generic(n,L,k1,q,L/2-d,L/2+d)
+  material2 = solve_generic(n,L,k2,q,0,L/2-d) + solve_generic(n,L,k2,q,L/2+d,L)
+
+  return material1+material2
+
+def grafico_forçantes(forc,):
+ 
+  
+  
 
 def main():
-    pass
+  grafico()
 
-
+    
 if __name__ == "__main__":
     main()
-
