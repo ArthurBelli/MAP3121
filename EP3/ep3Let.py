265b103f7dea6d15cdd2e64c61baa926cb32490a
Arthur Belli <29512865+ArthurBelli@users.noreply.github.com>
2022-07-13 20:03:20 -0300

    Tudo até o 4.3 funcionando
    Último exercício (materiais diferentes) não está funcionando e não sei porque

--- EP3/ep3Let.py
+++ EP3/ep3Let.py
@@ -16,16 +16,18 @@ def zero(x):
 # condutividade térmica
 
 def k(x):
-    return 1
+    return 3.6
 
-def q(Qp0, Qm0, sigma, theta, L, x):
-    q = Qp0*np.exp(-((x-L)/sigma)**2) - Qm0*(np.exp(-(x/theta)**2) + np.exp(-((x-L)/theta)**2))
+def que_const(x, Qp0=400, Qm0=200):
+    return Qp0-Qm0
+
+def que(x, Qp0=600, Qm0=300, sigma=0.1, theta=1, L=1):
+    q = Qp0*np.exp(-(x-L/2)**2/sigma**2) - Qm0*(np.exp(-(x)**2/theta**2) + np.exp(-((x-L)**2/theta**2)))
     return q
 
 # função de excitação do sistema
 def excitacao(x):
     return 12*x*(1-x)-2
-
 
 def phi(x, apoios, h, i): #funções da base do espaço gerado para o método dos elementos finitos
     if apoios[i-1] <= x <= apoios[i]: #primeira metade do spline é crescente
@@ -33,7 +35,6 @@ def phi(x, apoios, h, i): #funções da base do espaço gerado para o método do
     elif apoios[i] <= x <= apoios[i+1]: #segunda metade do spline é decrescente
         return (apoios[i+1]-x)/h
     return 0 # fora do intervalo
-
 
 def debug_phi(apoios, h, n):
     for k in range(1, n+1):
@@ -44,11 +45,9 @@ def calc_v(alphas, x, apoios, h, n):
     temp = float(sum([alphas[i-1]*phi(x, apoios, h, i) for i in range(1, n+1)]))
     return temp
 
-
 def fem(n, L, k, f):
     '''
     retorna a solução do problema normalizado em x in [0,1]
-
     n -> número de apoios
     L -> comprimento do chip
     k -> condutividade térmica do material
@@ -56,16 +55,16 @@ def fem(n, L, k, f):
     '''
     h = L/(n+1) #passo dos apoios
 
-    print(f"h = {h}")
+    #print(f"h = {h}")
 
     apoios = [i*h for i in range(n+2)]
-    print("apoios não normalizados: ")
-    print(apoios)
+    #print("apoios não normalizados: ")
+    #print(apoios)
 
     apoios_barr = [apoios[i]/L for i in range(n+2)]
-    print("apoios normalizados: ")
-    print(apoios_barr)
-    print("\n")
+    #print("apoios normalizados: ")
+    #print(apoios_barr)
+    #print("\n")
 
     #debug_phi(apoios_barr, h, n)
     #definição da matriz normal do MMQ 
@@ -104,7 +103,6 @@ def fem(n, L, k, f):
         v[k] = calc_v(alphas, eixox[k], apoios, h, n)
     return v #vetor solução normalizado
 
-
 def solve_generic(n, L, k, f, a, b): # calcula a temperatura pela equação f dada
     '''
     n -> número de apoios a serem utilizados
@@ -115,7 +113,6 @@ def solve_generic(n, L, k, f, a, b): # calcula a temperatura pela equação f da
     b -> condição de contorno no extremo L
     '''
 
-    h = L/(n+1)
     eixox = np.linspace(0, L, 100)
     v = fem(n, L, k, f)
     u = [0]*len(eixox)
@@ -123,35 +120,41 @@ def solve_generic(n, L, k, f, a, b): # calcula a temperatura pela equação f da
         u[i] = (v[i] + a + (b-a)*eixox[i]/L)
     return u #vetor solução individualizado
 
-
 def erro_max(u_teor, u_fem): # devolve o erro maximo entre o valor encontrado pelo fem e o teórico
     erro = u_teor - u_fem
     erro_m = np.max(erro)
-
     return erro_m
 
-def forcantes(q,L,n):
-  k=3.6
-  return fem(n,L,k,q)
+'''
+parte que está dando problema
+'''
+def k_mix(x, L=1, d=0.05):
+    if 0 <= x <= L/2 - d:
+        return 60
+    elif L/2 - d <= x <= L/2 + d:
+        return 3.6
+    elif L/2 + d <= x <= L:
+        return 60
+    return 0
 
-def mat_varia(q,L,n,d):
+
+def mat_varia(q,L,n):
   
-  k1=3.6
-  k2=60
-
-  material1= solve_generic(n,L,k1,q,L/2-d,L/2+d)
-  material2 = solve_generic(n,L,k2,q,0,L/2-d) + solve_generic(n,L,k2,q,L/2+d,L)
-
-  return material1+material2
-
-def grafico_forçantes(forc,):
- 
-  
-  
-
+  material21 = solve_generic(n, L, k_mix, q, 0, 0)
+  return material21
+'''
+fim da parte que está dando problema
+'''
 def main():
-  grafico()
+  n = 50
+  L = 1
+  #u = solve_generic(n, L, k, que_const, 0, 0)
+  #print(u)
+  u = mat_varia(que_const, L, n)
+  print(u)
 
     
 if __name__ == "__main__":
     main()
+
+
